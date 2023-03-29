fcac3bcaa2bac3514952cdfd72edd6ece73debe3
Moises Cermeno <mo.frc08@gmail.com>
2023-03-28 23:22:38 -0600

    feat: adding port and cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`running on ${port}`);
+  });
 }
 bootstrap();
